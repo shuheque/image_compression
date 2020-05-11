2072bc352ce14f8bc9c2be811f7e0cb0793955ed
gingerluo@qq.com <gingerluo.qq.com>
2020-05-11 14:31:59 +0800

    test

--- compress_image.py
+++ compress_image.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import os
+import os.path as opth
 import sys
 
 
@@ -13,25 +14,28 @@ def img_file(image_location):
 
 
 def get_compressed_file_name(file_location):
+    file_location = file_location.lower()
     if "compressed" in file_location:
         return file_location
     else:
         strip_extension = file_location.replace(".jpg", "")
         strip_extension = strip_extension.replace(".png", "")
+        strip_extension = strip_extension.replace(".jpeg", "")
         compressed_file_name = "%s%s" % (strip_extension.replace(".jpg", ""),
                                          "compressed.jpg")
         return compressed_file_name
 
 
 def compress(max_size, file_location):
+    file_location = file_location.lower()
     compressed_img_file_loc = get_compressed_file_name(file_location)
     image, size = img_file(file_location)
-    image.save(compressed_img_file_loc,optimize=True,quality=85)
+    image.save(compressed_img_file_loc,optimize=True,quality=90)
     if size > max_size:
         compress(max_size, compressed_img_file_loc)
 
 
-def main():
+def main1():
     max_size = int(sys.argv[1])
     files = sys.argv[2:]
     for file_loc in files:
@@ -40,7 +44,18 @@ def main():
         compress(max_size, file_loc)
     print("All file compressed.")
 
-
+def main():
+    try:
+        max_size = int(sys.argv[1])
+    except:
+        max_size = 538000
+    for file_loc in os.listdir(os.getcwd()):
+        if opth.isfile(file_loc):
+            print("%s will be compressed to %s" % (file_loc,
+                get_compressed_file_name(file_loc)))
+            compress(max_size, file_loc)
+    print("All file compressed.")
+    
 
 if __name__ == '__main__':
     main()
